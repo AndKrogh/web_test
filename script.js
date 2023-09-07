96f28cf6323674b05350e89f4f50dea9bffb5a00
AndKrogh <anders.krogh99@gmail.com>
2023-09-07 14:07:32 +0200

    Navbar

---
+++ script.js
@@ -0,0 +1,13 @@
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+
+const hamburgerMenu = [hamburger, navMenu];
+
+hamburger.addEventListener('click', function(){
+    hamburgerMenu[0,1].classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', function() {
+    hamburgerMenu[0, 1].classList.remove("active");
+}));
